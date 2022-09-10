7cf7f4035f6c8f046c72852b2be98c4bd18b4b0c
MaX <kingofmadnes@gmail.com>
2022-09-10 16:12:48 +0300

    edit sending message

--- apps/cvsender/tasks.py
+++ apps/cvsender/tasks.py
@@ -9,12 +9,13 @@ User = get_user_model()
         
 @shared_task   
 def send_email(companies, text, send_from_email, cv): 
-    # for company in companies:
-    # companies_selected = company.get('email')
+    companies_selected = ''
+    for company in companies:
+        companies_selected += str(company.get('email'))
     subject = "CV"   
     from_email = settings.DEFAULT_FROM_EMAIL
     to = settings.DEFAULT_FROM_EMAIL
-    text_content  = str(text)+str(send_from_email)
+    text_content  = str(text)+" from "+str(send_from_email)+" to "+str(companies_selected)
     msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
     msg.attach_file(cv)
     msg.send()
